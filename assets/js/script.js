17277e6fa71cefa75239f37edf5f9482d871c39e
Mohammed Usaith Abbas.S <pcasd21cs029@gmail.com>
2023-03-04 04:10:16 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,6 +175,23 @@ window.onload = fadeOut;
 
 
 // disable developer mode
+// document.onkeydown = function (e) {
+//     if (e.keyCode == 123) {
+//         return true;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//         return true;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+//         return true;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//         return true;
+//     }
+//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//         return true;
+//     }
+// }
 
 
 // Start of Tawk.to Live Chat
@@ -182,7 +199,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/640314fd4247f20fefe3ed1e/1gqm0kag7';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
