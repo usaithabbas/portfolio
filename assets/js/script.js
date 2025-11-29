1c8660248fc9b929064e474989442a95f106418f
Mohammed Usaith Abbas.S <pcasd21cs029@gmail.com>
2025-11-29 18:42:04 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,12 +30,12 @@ $(document).ready(function () {
     });
 
     // smooth scrolling
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
-        }, 500, 'linear')
-    });
+    // $('a[href*="#"]').on('click', function (e) {
+    //     e.preventDefault();
+    //     $('html, body').animate({
+    //         scrollTop: $($(this).attr('href')).offset().top,
+    //     }, 500, 'linear')
+    // });
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
