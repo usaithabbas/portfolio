436b13d9b1d789cbff5a6a7e0d8ea53f3527c99c
Mohammed Usaith Abbas.S <pcasd21cs029@gmail.com>
2025-11-29 18:44:42 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,12 +30,12 @@ $(document).ready(function () {
     });
 
     // smooth scrolling
-    // $('a[href*="#"]').on('click', function (e) {
-    //     e.preventDefault();
-    //     $('html, body').animate({
-    //         scrollTop: $($(this).attr('href')).offset().top,
-    //     }, 500, 'linear')
-    // });
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 500, 'linear')
+    });
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
@@ -175,23 +175,23 @@ window.onload = fadeOut;
 
 
 // disable developer mode
-// document.onkeydown = function (e) {
-//     if (e.keyCode == 123) {
-//         return true;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//         return true;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//         return true;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//         return true;
-//     }
-//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//         return true;
-//     }
-// }
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return true;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return true;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return true;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return true;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return true;
+    }
+}
 
 
 // Start of Tawk.to Live Chat
